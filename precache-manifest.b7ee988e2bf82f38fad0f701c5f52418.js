33ebaba0ed700a3d7752ad155736265761ed68c7
Justin_Howard <zorro1rr@gmail.com>
2020-06-11 12:58:35 -0600

    Updates

---
+++ precache-manifest.b7ee988e2bf82f38fad0f701c5f52418.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c1cb80fd72ebdeff3aa40501f02cddc",
+    "url": "/grooving/index.html"
+  },
+  {
+    "revision": "887cb3d41b4022b76df7",
+    "url": "/grooving/static/css/main.8b458c43.chunk.css"
+  },
+  {
+    "revision": "82e12aa4ce164a42ff36",
+    "url": "/grooving/static/js/2.cc06fb84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grooving/static/js/2.cc06fb84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "887cb3d41b4022b76df7",
+    "url": "/grooving/static/js/main.cbd3b7ce.chunk.js"
+  },
+  {
+    "revision": "7e01724ed5204c220199",
+    "url": "/grooving/static/js/runtime-main.addb91ec.js"
+  },
+  {
+    "revision": "a81981d5bc6bb64e49a342639951fde0",
+    "url": "/grooving/static/media/background_photo_desktop2.a81981d5.jpg"
+  }
+]);
