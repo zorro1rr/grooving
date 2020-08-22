c33394fb2cea6436f0a12bd1e328c74e3ad3564b
Justin_Howard <zorro1rr@gmail.com>
2020-08-22 15:52:55 -0600

    Updates

---
+++ precache-manifest.c525418954d3c99c2405d6cb62afa5ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28228d461d2145bc9e513f1d6308ce4e",
+    "url": "/grooving/index.html"
+  },
+  {
+    "revision": "ff53d9a8317a148145cf",
+    "url": "/grooving/static/css/main.ad51313c.chunk.css"
+  },
+  {
+    "revision": "82e12aa4ce164a42ff36",
+    "url": "/grooving/static/js/2.cc06fb84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grooving/static/js/2.cc06fb84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff53d9a8317a148145cf",
+    "url": "/grooving/static/js/main.50051918.chunk.js"
+  },
+  {
+    "revision": "7e01724ed5204c220199",
+    "url": "/grooving/static/js/runtime-main.addb91ec.js"
+  }
+]);
